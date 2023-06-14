d80edf6cd26b86345f5a097d2a53eec423bad7ac
bingjie6 <bingjie6@staff.weibo.com>
2023-06-14 17:32:10 +0800

    避免vim-session写入

--- lua/custom/plugins/session.lua
+++ lua/custom/plugins/session.lua
@@ -25,22 +25,22 @@ vim.api.nvim_exec(
      let g:session_default_to_last=1
      let g:session_lock_enabled=0
      let g:session_swap_name = ""
-     let g:startify_session_dir = stdpath('data') . '/sessions'
-     let g:session_directory =  stdpath('data') . '/sessions'
+     let g:startify_session_dir = stdpath('data') . '/session'
+     " let g:session_directory =  stdpath('data') . '/sessions'
      function MySessionReload(name, bang) abort
        if g:session_swap_name != ""  && a:name == ""
          let s_session_name_old = g:session_swap_name
          let g:session_swap_name =  fnamemodify(v:this_session, ':t')
-         call xolox#session#open_cmd(s_session_name_old, a:bang, 'OpenSession')
+         call startify#session_load(a:bang, s_session_name_old)
        else
          let g:session_swap_name =  fnamemodify(v:this_session, ':t')
-         call xolox#session#open_cmd(a:name, a:bang, 'OpenSession')
+         call startify#session_load(a:bang, a:name)
        endif
        if &filetype == "php"
          exe "silent! LspRestart"
        endif
      endfunction
-     command! -bar -bang -nargs=? -complete=customlist,xolox#session#complete_names MyOpenSession  call MySessionReload(<q-args>, <q-bang>)
+     command! -bar -bang -nargs=? -complete=customlist,startify#session_list MyOpenSession  call MySessionReload(<q-args>, <q-bang>)
 ]],
   true
 )
