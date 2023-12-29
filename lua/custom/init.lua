2d2fc93d7b1f924bee37eff68afedfc04e3f87e1
bingjie6 <bingjie6@staff.weibo.com>
2023-12-29 16:20:55 +0800

    指定c99

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -60,6 +60,8 @@ vim.api.nvim_command('command! -nargs=1 LiveGrep lua require("telescope").extens
 
 vim.cmd("autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })")
 
+vim.api.nvim_command('let $CFLAGS = $CFLAGS . " -std=c99"')
+
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
